47e0dada3f1d63392dd82a00ca5f518876415771
Michel Pereira <michelpr1224@gmail.com>
2026-02-09 00:56:15 -0300

    feat: rotas para receitas e categorias

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ReceitaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,4 +22,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/categorias', [CategoriaController::class, 'index']);
+    Route::apiResource('receitas', ReceitaController::class);
 });
