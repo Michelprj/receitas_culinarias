6a690c38313663c92479d035cfded7baf91326fc
Michel Pereira <michelpr1224@gmail.com>
2026-02-11 22:57:33 -0300

    chore: ajustou arquivos docker para melhor separação

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -14,4 +14,10 @@ if grep -q 'APP_KEY=$' .env 2>/dev/null || [ -z "$APP_KEY" ]; then
   php artisan key:generate --no-interaction --force
 fi
 
+# Rodar migrations pendentes
+php artisan migrate --force --no-interaction
+
+# Rodar seeds
+php artisan db:seed --force --no-interaction
+
 exec php artisan serve --host=0.0.0.0 --port=8000
